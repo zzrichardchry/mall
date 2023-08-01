42cac628fa68330faaf21c9170b8354ca6761746
bobo_cat <zzrichardchry@icloud.com>
2023-07-31 21:17:25 -0400

    18 Rapid Dev - Configure and Test Basic CRUD (all pom changed to the default settings when created)

--- mall-product/src/test/java/com/rick/mall/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/rick/mall/product/MallProductApplicationTests.java
@@ -1,13 +1,25 @@
 package com.rick.mall.product;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.rick.mall.product.entity.BrandEntity;
+import com.rick.mall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class MallProductApplicationTests {
 
+    @Autowired
+    BrandService brandService;
+
     @Test
     void contextLoads() {
+
+        List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1));
+        list.forEach(System.out::println);
     }
 
 }
