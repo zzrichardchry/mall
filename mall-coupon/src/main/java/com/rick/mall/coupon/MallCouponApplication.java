45341a0ea5179f5481b034a6bb640607d6a29cd0
bobo_cat <zzrichardchry@icloud.com>
2023-08-29 22:34:53 -0400

    23 Add Nacos Config Center

--- mall-coupon/src/main/java/com/rick/mall/coupon/MallCouponApplication.java
+++ mall-coupon/src/main/java/com/rick/mall/coupon/MallCouponApplication.java
@@ -5,6 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * 1. 如何使用 Nacos 作为配置中心统一管理
+ * 1) 引入依赖，并且在 application 里面添加对应的配置字段
+ * 2) 创建一个 bootstrap.properties，新版 Spring Cloud 需要引入 spring-cloud-starter-bootstrap 依赖
+ * 3) 在配置中心配置一个 dataID 配置
+ * 4) 给应用名.properties 添加任何配置
+ * 5) 动态获取配置：@RefreshScope 和 @Value("${配置项的名字}")，配置中心的值后加载，会覆盖。
+ */
 @MapperScan("com.rick.mall.coupon.dao")
 @SpringBootApplication
 @EnableDiscoveryClient
--- mall-coupon/src/main/java/com/rick/mall/coupon/controller/CouponController.java
+++ mall-coupon/src/main/java/com/rick/mall/coupon/controller/CouponController.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 import com.rick.mall.common.utils.PageUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,11 +26,22 @@ import com.rick.mall.common.utils.R;
  * @email rick@gmail.com
  * @date 2023-08-24 16:39:56
  */
+@RefreshScope
 @RestController
 @RequestMapping("coupon/coupon")
 public class CouponController {
     @Autowired
     private CouponService couponService;
+
+    @Value("${coupon.user.name}")
+    private String name;
+    @Value("${coupon.user.age}")
+    private String age;
+
+    @RequestMapping("/test")
+    public R test() {
+        return R.ok().put("name", name).put("age", age);
+    }
 
     @RequestMapping("/member/list")
     public R memberCoupons() {
