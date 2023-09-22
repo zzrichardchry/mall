015950edc4075d513fc5deeb49a7a669d9621eb3
bobo_cat <zzrichardchry@icloud.com>
2023-09-22 14:35:27 -0400

    45 Add SQL files & menu query functions: menus and corresponding children menus

--- mall-product/src/main/java/com/rick/mall/product/controller/CategoryController.java
+++ mall-product/src/main/java/com/rick/mall/product/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.rick.mall.product.controller;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,13 +32,14 @@ public class CategoryController {
     private CategoryService categoryService;
 
     /**
-     * 列表
+     * 查处所有分类以及子分类，以树形结构租床起来
      */
-    @RequestMapping("/list")
-    public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = categoryService.queryPage(params);
+    @RequestMapping("/list/tree")
+    public R list(){
 
-        return R.ok().put("page", page);
+        List<CategoryEntity> entities = categoryService.listWithTree();
+
+        return R.ok().put("data", entities);
     }
 
 
--- mall-product/src/main/java/com/rick/mall/product/service/impl/CategoryServiceImpl.java
+++ mall-product/src/main/java/com/rick/mall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,12 @@
 package com.rick.mall.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -24,6 +29,35 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        // 1. 查出所有分类
+        // baseMapper = ServiceImpl<CategoryDao, CategoryEntity> 里面的 CategoryDao
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+
+        // 2. 组装成父子的树形结构
+        // 2.1 找到所有的一级分类
+        List<CategoryEntity> level1Menu = entities.stream()
+                .filter(menu -> menu.getParentCid() == 0)
+                .peek(menu -> menu.setChildren(getChildren(menu, entities)))
+                .sorted(Comparator.comparingInt(m -> (m.getSort() == null ? 0 : m.getSort())))
+                .collect(Collectors.toList());
+
+        return entities;
+    }
+
+    // 递归查找所有菜单的子菜单
+    private List<CategoryEntity> getChildren(CategoryEntity root, List<CategoryEntity> entities) {
+        List<CategoryEntity> children = entities.stream()
+                .filter(m -> m.getParentCid() == root.getCatId())
+                // 1. 找到子菜单
+                .peek(m -> m.setChildren(getChildren(m, entities)))
+                // 2. 菜单的排序
+                .sorted(Comparator.comparingInt(m -> (m.getSort() == null ? 0 : m.getSort())))
+                .collect(Collectors.toList());
+        return children;
     }
 
 }
