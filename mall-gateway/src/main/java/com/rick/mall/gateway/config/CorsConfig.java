ca73a222910f7821013269d90e9012079cc79459
bobo_cat <zzrichardchry@icloud.com>
2023-09-25 18:19:13 -0400

    47 Fix Cors Problems, added a filter in mall-gateway and delete Cors files in renren-fast

---
+++ mall-gateway/src/main/java/com/rick/mall/gateway/config/CorsConfig.java
@@ -0,0 +1,32 @@
+package com.rick.mall.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+/**
+ * @Author zhuhaotian
+ * @Date 2023/9/25
+ */
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        // 1. 配置跨域
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOriginPattern("*");
+        corsConfiguration.setAllowCredentials(true); // 允许 cookie
+
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(source);
+    }
+
+}
